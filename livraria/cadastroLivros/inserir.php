31651e5966dba144ff8f4e53f2c3ad520b7a43e4
PEDRO HENRIQUE FIGUEIREDO <pedro.hfigueiredo@senacsp.edu.br>
2023-06-01 21:35:35 -0300

    Atualização

---
+++ livraria/cadastroLivros/inserir.php
@@ -0,0 +1,23 @@
+<?php
+include_once("conexxao.php");
+if($_POST){
+    $nome = $_POST['nome'];
+    $autor = $_POST['autor'];
+    $preco = $_POST['preco'];
+    $categoria = $_POST['categoria'];
+    $isbn = $_POST['isbn'];
+    $ano = $_POST['ano'];
+    $codigoEditora = $_POST['codigoEditora'];
+    $estoque = $_POST['estoque'];
+    $sql = "INSERT INTO livro (editora_cod_editora,estoque_cod_editora,nome_livro,auto_livro,preco_livro,categoria,isbn,ano) VALUES ($codigoEditora,$estoque,'$nome','$autor','$preco','$categoria','$isbn','$ano')";
+    if($mysql->query($sql)){
+        header("Location: cadastroLivros.html");
+
+    }else{
+        echo "Error:" . $sql . ":-" . mysqli_error($mysql);
+    }
+}
+
+
+
+?>
---
+++ poo.php
@@ -0,0 +1,50 @@
+<!-- Estrutura Classes -->
+<!-- Sintaxe para criar uma classe -->
+<!-- Class, seguida do nome da classe e um par de chaves ("{}") -->
+<?php
+class MinhaClasse
+{
+    //Propriedades e métodos da Classe vem aqui
+}
+?>
+<!-- Intanciada e guarda variavel usando a palavra chave new -->
+
+$obj = new MinhaClasse:
+
+<!-- Visualizar conteúdo da classe, usamos var_dump() -->
+
+<!-- Exemplo completo de class e instancia de Objeto recebendo valor mini -->
+
+<?php
+class MinhaClass
+{
+    //Propriedades e métodos da classse vem aqui
+}
+$obj = new MinhaClass;
+var_dump($obj);
+?>
+<!-- // Definindo as Propriedades da classe -->
+<!-- // Usamos as propriedades, que são variáveis especificas a classe -->
+<!-- Funcionalidade particulares que os objetos serão capazes de executar, são definidas dentro das classes na forma de metodos -->
+
+<?php
+class MinhaClass{
+    public $prop1 = "Sou um propriedade de classe!";
+
+}
+$prop1 = new MinhaClass;
+var_dump($prop1);
+
+?>
+
+<!-- Para ver o valor da propriedade -->
+<!-- Echo $obj->prop1 -->
+<!-- o uso da flecha (->) e um construtor de POO -->
+
+<?php
+class MinhaClass{
+    public $obj = "Sou uma propriedade de classe"
+}
+$obj = new MinhaClass;
+var_dump($sql);
+?>
